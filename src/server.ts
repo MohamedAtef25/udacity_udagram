f08f81c899e0c77bd0cb971a7a4606a60c32ae6e
Mohamed Atef <mohamedatef6@gmail.com>
2022-09-03 15:40:12 +0200

    Fixing non-working methods.

--- src/server.ts
+++ src/server.ts
@@ -31,11 +31,10 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   app.get('/filteredimage', async (req: Request, res: Response) => {
-    const {image_url} = req.query.image_url.toString();
+    let image_url:string = req.query.image_url;
     if(!image_url){
       res.status(400).send('Image url not found');
     }
-    
     const filtered = await filterImageFromURL(image_url);
 
     res.status(200).sendFile(filtered, () => {deleteLocalFiles([filtered])})
